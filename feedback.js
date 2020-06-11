42d47fe1a55a095e9ef5bf69563b4bf72e4e2d41
Athiappan <athiappan6@gmail.com>
2020-06-11 16:04:17 +0530

    My First Commit
    My First Commit

---
+++ feedback.js
@@ -0,0 +1,18 @@
+function doFirst(){
+    var button=document.getElementById("button");
+    button.addEventListener("click",saveCrap,false);
+}
+function saveCrap(){
+    var one=document.getElementById("one").value;
+var two=document.getElementById("two").value;
+sessionStorage.setItem(one,two);
+display(one);
+}
+function display(one){
+    var rightbox=document.getElementById("rightbox");
+    var two=sessionStorage.getItem(one);
+    rightbox.innerHTML="Entered Data <br/>"+"Name: "+one + "<br/>Rating: "+ two;
+
+}
+
+window.addEventListener("load",doFirst,false);
